2b9ed5f02de3e5c9a0dc678274fca30e17ec6e22
Sushil97 <sushil500530@gmail.com>
2023-10-13 18:35:47 +0600

    project set with tailwindcss and daisyui pluggin

---
+++ src/Router/Root.jsx
@@ -0,0 +1,13 @@
+
+
+const Root = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold text-center my-5">
+                This is Header
+            </h1>
+        </div>
+    );
+};
+
+export default Root;
