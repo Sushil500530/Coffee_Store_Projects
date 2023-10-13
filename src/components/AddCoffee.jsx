0840609a8feffa44f718cacef10348dac798044b
Sushil97 <sushil500530@gmail.com>
2023-10-13 20:14:28 +0600

    add to handleAddCoffee for get input value

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,8 +1,23 @@
 
 const AddCoffee = () => {
+    const handleAddCoffee = e => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const supplier = form.supplier.value;
+        const chef = form.chef.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+        const newCoffee = {name,supplier,chef,taste,details,photo,category};
+        console.log(newCoffee);
+
+        
+    }
     return (
         <div className="container mx-auto">
-            <div className="w-full h-screen bg-[#F4F3F0] px-12">
+            <div className="w-full lg:h-screen h-[1200px] bg-[#F4F3F0] px-12">
                     <div className="space-y-5 pb-5">
                     <h1 className="text-3xl font-bold text-center pt-12">
                     Add New Coffee </h1>
@@ -11,7 +26,7 @@ const AddCoffee = () => {
                     </p>
                     </div>
                     <div className="bg-[#F4F3F0]">
-                        <form className="card-body  text-xl font-semibold">
+                        <form onSubmit={handleAddCoffee} className="card-body  text-xl font-semibold">
                             <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
                                 <div className=" w-full space-y-4 py-3">
                                     <div className="form-control">
@@ -24,13 +39,13 @@ const AddCoffee = () => {
                                         <label className="label">
                                             <span className="label-text">Supplier</span>
                                         </label>
-                                        <input type="text" name="Supplier" placeholder="Enter Cofee Supplier" className="input input-bordered" required />
+                                        <input type="text" name="supplier" placeholder="Enter Cofee Supplier" className="input input-bordered" required />
                                     </div>
                                     <div className="form-control">
                                         <label className="label">
                                             <span className="label-text">Category</span>
                                         </label>
-                                        <input type="text" name="Category" placeholder="Enter Cofee Category" className="input input-bordered" required />
+                                        <input type="text" name="category" placeholder="Enter Cofee Category" className="input input-bordered" required />
                                     </div>
                                 </div>
                                 <div className=" w-full space-y-4 py-3">
@@ -38,19 +53,19 @@ const AddCoffee = () => {
                                         <label className="label">
                                             <span className="label-text">Chef</span>
                                         </label>
-                                        <input type="text" name="name" placeholder="Enter Cofee Chef" className="input input-bordered" />
+                                        <input type="text" name="chef" placeholder="Enter Cofee Chef" className="input input-bordered" />
                                     </div>
                                     <div className="form-control">
                                         <label className="label">
                                             <span className="label-text">Taste</span>
                                         </label>
-                                        <input type="text" name="Taste" placeholder="Enter Cofee Taste" className="input input-bordered" required />
+                                        <input type="text" name="taste" placeholder="Enter Cofee Taste" className="input input-bordered" required />
                                     </div>
                                     <div className="form-control">
                                         <label className="label">
                                             <span className="label-text">Details</span>
                                         </label>
-                                        <input type="text" name="Details" placeholder="Enter Cofee Details" className="input input-bordered" required />
+                                        <input type="text" name="details" placeholder="Enter Cofee Details" className="input input-bordered" required />
                                     </div>
                                 </div>
                             </div>
@@ -58,7 +73,7 @@ const AddCoffee = () => {
                                 <label className="label">
                                     <span className="label-text">Photo</span>
                                 </label>
-                                <input type="url" name="Photo" placeholder="Photo URL" className="input input-bordered" required />
+                                <input type="url" name="photo" placeholder="Photo URL" className="input input-bordered" required />
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn bg-[#D2B48C]">Add Coffee</button>
