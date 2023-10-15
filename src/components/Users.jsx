0065b8d69c94bdc5cf5f79e6c321d876ed1622ab
Sushil97 <sushil500530@gmail.com>
2023-10-15 12:08:40 +0600

    add to delete mathod for users

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,12 +1,50 @@
-import { lazy } from "react";
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
-
+import { AiFillDelete } from 'react-icons/ai';
+import Swal from 'sweetalert2';
 
 const Users = () => {
     const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers);
+
+    const handleDelete = _id => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/user/${_id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your Coffee has been deleted.',
+                                'success'
+                            )
+
+                            const remaining = users.filter(user => user._id !== _id);
+                            setUsers(remaining)
+                        }
+                    })
+            }
+        })
+    }
+
+
+
     return (
         <div className="container mx-auto text-center">
-           <h3>users comming soon {loadedUsers.length}</h3>
+            <h3>users comming soon {loadedUsers.length}</h3>
             <div className="overflow-x-auto">
                 <table className="table">
                     {/* head */}
@@ -15,18 +53,22 @@ const Users = () => {
                             <th></th>
                             <th>Email</th>
                             <th>CreatedAt</th>
+                            <th>Last Logged In</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                    {
-                        loadedUsers.map(user =>   <tr key={user._id}>
-                            <th>1</th>
-                            <td>{user.email}</td>
-                            <td>{user.createdAt}</td>
-                            <td>Blue</td>
-                        </tr> )
-                    }
+                        {
+                            users.map(user => <tr key={user._id}>
+                                <th>1</th>
+                                <td>{user.email}</td>
+                                <td>{user.createdAt}</td>
+                                <td>Blue</td>
+                                <td>
+                                    <button onClick={() => handleDelete(user._id)} className="btn bg-[#EA4744] hover:bg-[#930a07]"><AiFillDelete className='text-white text-xl'></AiFillDelete></button>
+                                </td>
+                            </tr>)
+                        }
                     </tbody>
                 </table>
             </div>
